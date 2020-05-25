e3f67da9759bc4f31b237a9439773ce2c8dc4da8
NorthonVinicios <northinho15@gmail.com>
2020-05-25 20:00:57 -0300

    feat: add refactors to SignupController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,29 @@
 export class SignUpController {
     handle(httpRequest: any): any{
-       return {
-           statusCode: 400,
-           body: new Error('missing param: name')
+        if(!httpRequest.body.name){
+            return {
+                statusCode: 400,
+                body: new Error('missing param: name')
 
-       } 
+                    } 
+                }
+        if(!httpRequest.body.email){
+            return {
+                statusCode: 400,
+                body: new Error('missing param: email')
+            }
+        } 
+        if(!httpRequest.body.password){
+            return {
+                statusCode: 400,
+                body: new Error('missing param: password')
+            }
+        } 
+        if(!httpRequest.body.passwordConfirmation){
+            return {
+                statusCode: 400,
+                body: new Error('missing param: password Confirmation')
+            }
+        } 
     }
 }
